2f826eeaea7dc98f69e9e41ab197609ae6c6ab2f
Genaro Di Stilio <genarodistilio@gmail.com>
2023-04-28 14:52:29 -0300

    Grammar

--- src/App.java
+++ src/App.java
@@ -8,61 +8,56 @@ public class App {
         Product product5 = new Product(1, "Fan", 35);
         Product product6 = new Product(4, "Spoon", 2);
         DoublyLinkedList lista = new DoublyLinkedList();
-        System.out.println("-----------------------------------------------------");
         System.out.println("");
-        System.out.println("Added: Table, code 11, price 50(Add first code number)");
+        System.out.println("Added: Table, code 11, price 50.");
         lista.insertInOrder(product1);/* Insert smaller than the first works */
 
-        System.out.println("Added: Chair, code 3, price 10 (Add smaller code number works)");
+        System.out.println("Added: Chair, code 3, price 10.");
         lista.insertInOrder(product2); /* Insert smaller than the first works */
 
-        System.out.println("Added: Carpet, code 12, price 30 (Add bigger code number works)");
+        System.out.println("Added: Carpet, code 12, price 30.");
         lista.insertInOrder(product3); /* Insert greater than the last works */
 
         System.out.println("Added: Keyboard, code 12, price 30 (Repeated code number, gives error)");
         lista.insertInOrder(product7); /* Insert a repeated code doesn't works */
 
-        System.out.println("Added: Pot, code 10, price 7 (Add in between code number works)");
+        System.out.println("Added: Pot, code 10, price 7.");
         lista.insertInOrder(product4); /* Insert in between works */
 
-        System.out.println("Added: Fan, code 1, price 35");
+        System.out.println("Added: Fan, code 1, price 35.");
         lista.insertInOrder(product5);
         System.out.println("");
 
         System.out.println("Now the Doubly Linked List looks like this:");
-        System.out.println("");
+        System.out.println("-----------------------------------------------------");
         lista.showList(); /* Show how the list is ordered by product code */
-        System.out.println("");
+        System.out.println("-----------------------------------------------------");
         System.out.println("Sorted by code number regardless of the order in which they were added.");
         System.out.println("");
         
-        
-        
-        
-        
        
         
-        System.out.println("-----------------------------------------------------");
         System.out.println("");
-        System.out.println("Deleting Carpet, code 12, delete tail product works:");
+        System.out.println("First, we delete Carpet to show that removing the tail works:");
         lista.deleteProduct(product3); /* Delete the last works */    
+        System.out.println("-----------------------------------------------------");
         lista.showList();
+        System.out.println("-----------------------------------------------------");
 
         System.out.println("");
         
-        System.out.println("Deleting Pot, code 10, delete products in between works:");
-        System.out.println("Now the Doubly Linked List looks like this:");
-        System.out.println("");
+        System.out.println("Then, we delete Pot to show that removing items in between works:");
         lista.deleteProduct(product4); /* Delete in the middle works */
+        System.out.println("-----------------------------------------------------");
         lista.showList();
+        System.out.println("-----------------------------------------------------");
 
         System.out.println("");
 
-        System.out.println("Deleting Fan, code 1, delete head product works:");
-        System.out.println("");
+        System.out.println("Finally, we delete Fan to show that removing the head works. ");
+        System.out.println("-----------------------------------------------------");
         lista.deleteProduct(product5); /* Delete the first works */
         lista.showList();
-        System.out.println("");
         System.out.println("-----------------------------------------------------");
         System.out.println("");
     }
